5fadb89f88c6622d5d6bf2be00a31afd630cb26c
Josh <joshuadbroadwater>
2016-08-25 09:18:16 -0500

    refactored .js files to jQuery

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,9 +1,10 @@
-var collectionItemTemplate = 
- '<div class="collection-album-container column fourth">'
-+'  <img src="assets/images/album_covers/01.png"/>'
-+'  <div class="collection-album-info caption">'
-+'      <p>'
-+'          <a class="album-name" href="album.html"> The Colors </a>'
+var buildCollectionItemTemplate = function() {
+var template = 
+'<div class="collection-album-container column fourth">'
++  '<img src="assets/images/album_covers/01.png"/>'
++  '<div class="collection-album-info caption">'
++      '<p>'
++'           <a class="album-name" href="album.html"> The Colors </a>'
 +'          <br/>'
 +'          <a href="album.html"> Pablo Picasso </a>'
 +'          <br/>'
@@ -11,17 +12,21 @@ var collectionItemTemplate =
 +'          <br/>'
 +'        </p>'
 +'      </div>'
-+'  </div>'
++'</div>'
 ;
+   return $(template); 
+};
 
-window.onload = function() {
+$(window).load = function() {
     //#1
-    var collectionContainer = document.getElementsByClassName('album-covers')[0];
+    var $collectionContainer = $('.album-covers');
     //#2
-    collectionContainer.innerHTML = '';
+$collectionContainer.empty();
     // #3
     for (var i =0; i < 12; i++) {
-        collectionContainer.innerHTML += collectionItemTemplate;
-    }
+        var $newThumbnail = buildCollectionItemTemplate();
+        
+        $collectionContainer.append($newThumbnail);
 }
 
+};
--- scripts/landing.js
+++ scripts/landing.js
@@ -1,24 +1,35 @@
-var pointsArray = document.getElementsByClassName('point');
+//var pointsArray = document.getElementsByClassName('point');
 
-var revealPoint = function(point) {
-    point.style.opacity = 1;
-    point.style.transform = "scaleX(1) translateY(0)";
-    point.style.msTransform = "scaleX(1) translateY(0)";
-    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+
+  //  point.style.transform = "scaleX(1) translateY(0)";
+//    point.style.msTransform = "scaleX(1) translateY(0)";
+//    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+//};
+
+var animatePoints = function() {
+    var revealPoint = function() {
+    $(this).css({
+       opacity: 1,
+        transform: 'scaleX(1) translateY(0)'
+    });
 };
-
-var animatePoints = function(param) {
-    forEach(param, revealPoint);
+    $.each($('point'), revealPoint);
 };
   
-window.onload = function() {
-   if (window.innerHeight > 950) {
-       animatePoints(pointsArray);
+$(window).load(function() {
+   if ($(window).height() > 950) {
+       animatePoints();
    }
     
-    window.addEventListener('scroll', function(event) {
-        if (pointsArray[0].getBoundingClientRect().top <= 500) {
-            animatePoints(pointsArray);
+
+   
+    
+    
+$(window).scroll(function(event) {
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+    
+    if ($(window).scrollTop() >= scrollDistance) {
+        animatePoints();
         }
     });
-}
+ });
